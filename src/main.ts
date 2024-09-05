297d22c14b054bfdda76b8e946c122f39955f9a2
ChrstianH <henschel_christian@web.de>
2024-09-05 11:04:43 +0200

    TypeScript Teil 1

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,45 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { IProduct, IRating, Category } from "./interfaces/IProduct";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const BASE_URL = "https://fakestoreapi.com";
+const PRODUCT_URL = `${BASE_URL}/products`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// EIngabefelder
+const searchElement = document.getElementById("search") as HTMLInputElement;
+const sortElement = document.getElementById("sort") as HTMLSelectElement;
+
+// Filterbuttons
+const electronicsFlt = document.getElementById(
+  "electronics"
+) as HTMLButtonElement;
+const jeweleryFlt = document.getElementById("jewelery") as HTMLButtonElement;
+const menClothingFlt = document.getElementById(
+  "menClothing"
+) as HTMLButtonElement;
+const womenClothingFlt = document.getElementById(
+  "womenClothing"
+) as HTMLButtonElement;
+
+// Sections
+const cartContent = document.getElementsByClassName(
+  "cart-content"
+)[0] as HTMLElement;
+
+// bei leerem Search-Feld alles ausgeben
+if (searchElement.value.length === 0) {
+  fetch(PRODUCT_URL)
+    .then((response: Response) => {
+      return response.json();
+    })
+    .then((data: any) => {
+      data.forEach((product: IProduct) => {
+        const productCard = document.createElement("div") as HTMLDivElement;
+        const productDiv = document.createElement("div") as HTMLDivElement;
+        const priceDiv = document.createElement("div") as HTMLDivElement;
+        productCard.appendChild(productDiv);
+        productCard.appendChild(priceDiv);
+        cartContent.appendChild(productCard);
+      });
+    })
+    .catch((error: Error) => console.error(error.message));
+}
